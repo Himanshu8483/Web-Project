815dda9776500f6b1a9e36725e5445ab819700ac
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-01-16 00:27:44 +0530

    all

--- vanta.js
+++ vanta.js
@@ -22,3 +22,14 @@
       scaleMobile: 1.00,
       points: 20.00
     })
+  VANTA.NET({
+    el: "#appointment",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      points: 20.00
+    })
