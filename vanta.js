042f1f65eaabb8623760be015c69365bc0f5e9b1
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-01-24 21:55:18 +0530

    all

--- vanta.js
+++ vanta.js
@@ -22,16 +22,3 @@
       scaleMobile: 1.00,
       points: 20.00
     })
-
-    // VANTA.TOPOLOGY({
-    //   el: "#sec2",
-    //   mouseControls: true,
-    //   touchControls: true,
-    //   gyroControls: false,
-    //   minHeight: 750.00,
-    //   minWidth: 200.00,
-    //   scale: 1.00,
-    //   scaleMobile: 1.00,
-    //   color: 0x861c73,
-    //   backgroundColor: 0xffffff
-    // })
