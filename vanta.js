fb3e7f6991df2e2e5719e4e26cdda0490a67faf8
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-01-25 22:16:08 +0530

    all

--- vanta.js
+++ vanta.js
@@ -8,6 +8,9 @@
         minWidth: 200.00,
         scale: 1.00,
         scaleMobile: 1.00,
+        color: 0x0c5d69,
+        color2: 0x0c5d69,
+        backgroundColor: 0x150505,
         points: 20.00
       })
 
@@ -20,5 +23,8 @@
       minWidth: 200.00,
       scale: 1.00,
       scaleMobile: 1.00,
+      color: 0x0c5d69,
+      color2: 0x0c5d69,
+      backgroundColor: 0x150505,
       points: 20.00
     })
